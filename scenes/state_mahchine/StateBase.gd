5aac87074d512357cbea09c3b30ea406c99bdb8b
Ye96516 <162856784+Ye96516@users.noreply.github.com>
2025-01-10 00:00:40 +0800

    引入状态机模版

---
+++ scenes/state_mahchine/StateBase.gd
@@ -0,0 +1,26 @@
+class_name StateBase extends Node
+
+@export var ans:AnimatedSprite2D
+@export var an:AnimationPlayer
+@export var player:Player
+
+var state_machine: StateMachine
+
+## 进入状态
+func enter() -> void:
+	pass
+
+## 退出状态
+func exit() -> void:
+	pass
+
+## 渲染帧触发
+func process_update(_delta: float) -> void:
+	pass
+
+## 物理帧触发
+func physical_process_update(_delta: float) -> void:
+	pass
+#hihuihnuiu,,jh
+#hhahah
+#怎么提交啊啊啊啊啊
